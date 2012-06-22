0673d5ec6bdb76c369026d1bb38b3a4b57600db3
Dana Chen <dana@danawyn.com>
2012-06-22 12:48:36 -0700

    Created Users model

---
+++ app/models/task.rb
@@ -0,0 +1,3 @@
+class Task < ActiveRecord::Base
+  attr_accessible :BlueAlgae, :description
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TodoList::Application.routes.draw do
+  resources :users
+
+  resources :tasks
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
